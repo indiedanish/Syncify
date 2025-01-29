ebb70cc2a12d2aa1123cbe1f376e2bb015608549
indiedanish <dbdanishbutt@gmail.com>
2025-01-29 19:29:51 +0500

    feat/project-init

---
+++ components/header.jsx
@@ -0,0 +1,94 @@
+import React from "react";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import Link from "next/link";
+import Image from "next/image";
+import { Button } from "./ui/button";
+import DarkMode from "./ui/dark-mode";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+import {
+  ChevronDown,
+  LayoutDashboard,
+  StarIcon,
+  FileText,
+  PenBox,
+  GraduationCap,
+} from "lucide-react";
+const header = () => {
+  return (
+    <header className="fixed top-0 w-full border-b bg-background/80 backdrop-blur-md z-50 supports-[backdrop-filter]:bg-background/60">
+      <nav className="container mx-auto py-3 h-16 flex item-center justify-between">
+        <Link href="/">
+          <Image
+            src="https://www.pngplay.com/wp-content/uploads/3/Samsung-Logo-Background-PNG-Image.png"
+            width={200}
+            height={30}
+          />
+        </Link>
+
+        <div className="flex items-center space-x-2 md:space-x-4 ">
+          <SignedIn>
+            <Link href="/dashboard">
+              <Button variant="outline">
+                <LayoutDashboard className="h-4 w-4" />
+
+                <span className="hidden md:block">Industry Insights</span>
+              </Button>
+            </Link>
+   
+
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button className="flex items-center gap-2">
+                <StarIcon className="h-4 w-4" />
+                <span className="hidden md:block">Growth Tools</span>
+                <ChevronDown className="h-4 w-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end" className="w-48">
+              <DropdownMenuItem asChild>
+                <Link href="/resume" className="flex items-center gap-2">
+                  <FileText className="h-4 w-4" />
+                  Build Resume
+                </Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem asChild>
+                <Link
+                  href="/ai-cover-letter"
+                  className="flex items-center gap-2"
+                >
+                  <PenBox className="h-4 w-4" />
+                  Cover Letter
+                </Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem asChild>
+                <Link href="/interview" className="flex items-center gap-2">
+                  <GraduationCap className="h-4 w-4" />
+                  Interview Prep
+                </Link>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+          </SignedIn>
+          <DarkMode />
+          <UserButton />
+          <SignedOut>
+            <SignInButton>
+            <Button variant="outline">
+                <span className="hidden md:block">Sign In</span>
+              </Button>
+            </SignInButton>
+          </SignedOut>
+        </div>
+      </nav>
+
+    </header>
+  );
+};
+
+export default header;
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ["www.pngplay.com", "https://randomuser.me"], 
+        remotePatterns: [
+            {
+              protocol: "https",
+              hostname: "randomuser.me",
+            },
+          ],
+    },
+};
 
 export default nextConfig;
