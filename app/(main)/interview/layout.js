28b7a7e6589cdf7051c78ef867df50b0816d5549
indiedanish <dbdanishbutt@gmail.com>
2025-01-31 15:11:36 +0500

    fix/scrollbar-css

--- app/(main)/interview/layout.js
+++ app/(main)/interview/layout.js
@@ -4,6 +4,11 @@ import { BarLoader } from "react-spinners";
 export default function Layout({ children }) {
   return (
     <div className="px-5">
+      <div className="flex items-center justify-between mb-5">
+        <h1 className="text-6xl font-bold gradient-title">
+          Interview Preparation
+        </h1>
+      </div>
       <Suspense
         fallback={<BarLoader className="mt-4" width={"100%"} color="gray" />}
       >
